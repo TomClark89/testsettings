2ae83daf5003c743e87bb9a7fee2b92faa929b99
Tom Clark <tom.wil.clark@gmail.com>
2016-05-09 23:14:59 +0100

    got it working taking user and pass from config file

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,11 @@
+import configparser
+config = configparser.ConfigParser()
+
+config.read('settings.ini')
+secretstuff = config['SECRETSTUFF']
+
+
+for key in secretstuff:
+    print ('    ' + key + ':' + secretstuff[key])
+
 print('hello world')
